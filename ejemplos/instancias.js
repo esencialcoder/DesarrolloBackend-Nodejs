a137428fd160e6cd3bf58ead6b0d97d54a7524a1
Juan Manuel <juanmanuel31@hotmail.com>
2023-02-16 20:43:10 +0100

    Creo archivo instancias.js

---
+++ ejemplos/instancias.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// crear una función para usarla como constructor de objetos
+function Fruta(nombre) {
+    this.nombre = nombre;
+    this.saluda = function() {
+        console.log('Hola, soy', this.nombre);
+    }
+};
+
+const limon = new Fruta('limon');
+
+console.log(limon);
+
+
+// limon.saluda();
